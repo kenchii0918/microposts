a7f968bb1652d4a59ac48edbae2128edde2736ee
kenta-nishimura <kenchii365@gmail.com>
2021-07-14 07:31:13 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,10 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
+            
+            {{--ユーザー登録ページのリンク--}}
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' =>'btn btn-primary'}) !!}
+
         </div>
     </div>
 @endsection
