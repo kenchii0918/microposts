08cbe52ab8771f71e974ec1cbf1209a517a5f80c
kenta-nishimura <kenchii365@gmail.com>
2021-07-14 08:14:19 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the Microposts</h1>
-            
-            {{--ユーザー登録ページのリンク--}}
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' =>'btn btn-primary'}) !!}
-
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                
+                {{--ユーザー登録ページのリンク--}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' =>'btn btn-primary'}) !!}
+    
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
